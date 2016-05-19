8a244ce563766ecd8b33eba7aa9582e2ccbd8b1b
Craig <craigwilliams84@gmail.com>
2016-05-19 16:35:11 +0100

    #1 Committed missing match result command class

---
+++ command_gateway/src/main/java/com/capgemini/scores/gateway/message/MatchResultCommand.java
@@ -0,0 +1,18 @@
+package com.capgemini.scores.gateway.message;
+
+import com.capgemini.scores.gateway.domain.MatchResult;
+
+public class MatchResultCommand implements Command<MatchResult> {
+
+    private MatchResult matchResult;
+    
+    public MatchResultCommand(MatchResult matchResult) {
+        this.matchResult = matchResult;
+    }
+    
+    @Override
+    public MatchResult getPayload() {
+        return matchResult;
+    }
+
+}
