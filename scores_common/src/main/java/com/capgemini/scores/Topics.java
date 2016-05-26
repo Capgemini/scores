2b64a7da36648d0a25680b818e1241bacacb0442
Craig <craigwilliams84@gmail.com>
2016-05-26 15:44:13 +0100

    #7 Stagger port numbers for services and changed the match result event topic name

--- scores_common/src/main/java/com/capgemini/scores/Topics.java
+++ scores_common/src/main/java/com/capgemini/scores/Topics.java
@@ -4,7 +4,7 @@ public class Topics {
     
     public static final String MATCH_RESULT_COMMAND = "cmdMatchResult";
 
-    public static final String MATCH_RESULT_EVENT = "matchResult";
+    public static final String MATCH_RESULT_EVENT = "evtMatchResult";
     
     public static final String CREATE_LEAGUE_TABLE_COMMAND = "cmdCreateLeagueTable";
 
